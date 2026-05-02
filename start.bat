05caf8aeeeced87053db417cca1e3016897b2f83
Jorge-Canales <jorgealbertocg@hotmail.com>
2026-05-02 15:30:04 -0600

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Ir a la carpeta donde está este .bat
+cd /d "%~dp0"
+
+REM Iniciar el servidor
+start cmd /k node server.js
+
+REM Esperar a que arranque
+timeout /t 2 >nul
+
+REM Abrir en navegador (mejor localhost)
+start http://localhost:3000/controller.html
+start http://localhost:3000/overlay.html
